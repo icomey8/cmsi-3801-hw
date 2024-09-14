60c752f64d8d51efcb135dc646205d6da6ada63d
jiamese <80025619+jiamese@users.noreply.github.com>
2024-09-13 17:22:27 -0700

    Update exercises.lua

--- lua/exercises.lua
+++ lua/exercises.lua
@@ -24,7 +24,17 @@ function first_then_lower_case(strings, predicate)
 end
 
 -- Write your powers generator here
-
+function powers_generator(base, limit)
+    return coroutine.create(function()
+        local value = 1
+        local exponent = 0
+        while value <= limit do
+            coroutine.yield(value)
+            exponent = exponent + 1
+            value = base ^ exponent
+        end
+    end)
+end
 -- Write your say function here
 
 -- Write your line count function here
@@ -45,8 +55,66 @@ function meaningful_line_count(file)
 end
 
 -- Write your Quaternion table here
--- Quarternion (function (class)
---   class.new = function ()
---   end
+Quaternion = {}
+Quaternion.__index = Quaternion
 
--- end)({})
+-- Constructor
+function Quaternion.new(a, b, c, d)
+    local self = setmetatable({}, Quaternion)
+    self.a = a or 0  -- real part
+    self.b = b or 0  -- i component
+    self.c = c or 0  -- j component
+    self.d = d or 0  -- k component
+    return self
+end
+
+-- String representation of quaternion
+function Quaternion:__tostring()
+    local parts = {}
+    if self.a ~= 0 or (self.a == 0 and self.b == 0 and self.c == 0 and self.d == 0) then
+        table.insert(parts, tostring(self.a))
+    end
+    if self.b ~= 0 then
+        table.insert(parts, (self.b == 1 and "" or self.b == -1 and "-" or self.b) .. "i")
+    end
+    if self.c ~= 0 then
+        table.insert(parts, (self.c == 1 and "" or self.c == -1 and "-" or self.c) .. "j")
+    end
+    if self.d ~= 0 then
+        table.insert(parts, (self.d == 1 and "" or self.d == -1 and "-" or self.d) .. "k")
+    end
+    return table.concat(parts, "+"):gsub("%+%-", "-")
+end
+
+-- Conjugate of a quaternion
+function Quaternion:conjugate()
+    return Quaternion.new(self.a, -self.b, -self.c, -self.d)
+end
+
+-- Addition of two quaternions
+function Quaternion.__add(q1, q2)
+    return Quaternion.new(q1.a + q2.a, q1.b + q2.b, q1.c + q2.c, q1.d + q2.d)
+end
+
+-- Multiplication of two quaternions 
+function Quaternion.__mul(q1, q2)
+    return Quaternion.new(
+        q1.a * q2.a - q1.b * q2.b - q1.c * q2.c - q1.d * q2.d,
+        q1.a * q2.b + q1.b * q2.a + q1.c * q2.d - q1.d * q2.c,
+        q1.a * q2.c - q1.b * q2.d + q1.c * q2.a + q1.d * q2.b,
+        q1.a * q2.d + q1.b * q2.c - q1.c * q2.b + q1.d * q2.a
+    )
+end
+
+-- Coefficients list
+function Quaternion:coefficients()
+    return {self.a, self.b, self.c, self.d}
+end
+
+-- Equality check 
+function Quaternion.__eq(q1, q2)
+    return q1.a == q2.a and q1.b == q2.b and q1.c == q2.c and q1.d == q2.d
+end
+
+return Quaternion
+
